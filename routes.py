9d53921e3397a250eb00fed7b5616e81168f8ca3
jeffthemaximum <frey.maxim@gmail.com>
2015-07-17 20:37:27 -0400

    various undocumented commits

--- routes.py
+++ routes.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pudb, random
+from random import shuffle
 
 app = Flask(__name__)
+my_dict = {}
+clicks = {}
 
 @app.route('/')
 def home():
@@ -10,10 +13,27 @@ def home():
 @app.route('/tiny', methods=['POST'])
 def tiny():
 	url = request.form['url']
-	num = random.randint(10000, 99999)
+	short = []
 
+	for i in range(0,2):
 
-	return render_template('tinyURL.html', url=url)
+		upper = random.randint(65, 90)
+		short.append(str(unichr(int(upper))))
+		lower = random.randint(97, 122)
+		short.append(str(unichr(int(lower))))
+		num = random.randint(10, 99)
+		short.append(str(num))
+
+	shuffle(short)
+	new_url = "".join(short)
+	my_dict[new_url] = url
+
+	return render_template('tinyURL.html', url=url, new_url=new_url)
+
+@app.route('/<key>', methods=['GET'])
+def redirect_tiny(key):
+	long_url = 'http://' + my_dict[key]
+	return redirect(long_url, code=302)
 
 if __name__ == '__main__':
 	app.run(debug=True)
